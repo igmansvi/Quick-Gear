36c77aae5ff5a7ab94e46e5174ff368ca417cd39
Mansvi <itsmansvi@gmail.com>
2025-04-03 15:50:31 +0530

    minor fixes

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['user_role'] = $user['role'];
 
                 if ($user['role'] === 'admin') {
-                    header("Location: admin/dashboard.php");
+                    header("Location: admin.php");
                 } else {
                     header("Location: index.php");
                 }
